50aeae9cd2964069a8b97819c8e048f1e9eb483d
Umapathy <umapathy.girirajan@one.verizon.com>
2016-07-17 12:11:48 +0530

    Hydra App.

---
+++ HydraService/HydraService/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using CsvHelper;
+using System.Configuration;
+
+namespace HydraService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App app = new App();
+            string FilePath = ConfigurationManager.AppSettings["CSVFilePath"];
+            #region Import Keyword List
+            //app.UpdateKeywordListFromCSV(FilePath);
+            #endregion
+            #region Process Rules
+            List<FieldInfo> CSVDataList = app.GetCSVData(FilePath);
+            var SummaryData = CSVDataList.FindKeyword("Summary", 0);
+            var ClassAData = CSVDataList.FindKeywordAfterSpecificLine("Class A-2B Notes", SummaryData.LineNumber, 1);
+            #endregion
+        }
+    }
+}
